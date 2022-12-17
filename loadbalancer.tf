6d81c1734d2d8038ffee945582325b850d646ea0
MELISSA GARCIA ANGEL <garciaangelmelissa@gmail.com>
2022-12-16 22:25:02 -0500

    Correcting health checks.

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -4,16 +4,9 @@ resource "aws_lb_target_group" "LoadBalancer" {
   port     = 80
   protocol = "TCP"
   vpc_id   = aws_vpc.VPC1.id
+  
   health_check {
-    enabled = true
-    healthy_threshold = 3
-    interval = 10
-    matcher = 200
-    path = "/"
-    port = "traffic-port"
-    protocol = "HTTP"
-    timeout = 3
-    unhealthy_threshold = 2
+    protocol = "TCP"
   }
 }
 
